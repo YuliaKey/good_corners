6ea35d6bdf8adbc7ea7ba023ce9bf684d617cc7b
Yuliia Kolomiets <ykolomiets@mac100003.home>
2023-11-30 15:52:10 +0100

    feat : add upload image functionality to frontend side of the project

--- frontend/src/pages/ad/new.tsx
+++ frontend/src/pages/ad/new.tsx
@@ -29,12 +29,16 @@ const NewAd = () => {
     formState: { errors },
   } = useForm<Inputs>();
 
+  const [file, setFile] = useState<File>();
+  const [imageURL, setImageURL] = useState<String>();
   const [categories, setCategories] = useState<CategoryType[]>([]);
 
   const [
     createNewAd,
     { data: createAdData, loading: createAdLoading, error: createAdError },
   ] = useMutation(CREATE_NEW_AD);
+
+  console.log("image", imageURL);
 
   const onSubmit: SubmitHandler<Inputs> = async (data) => {
     console.log(data);
@@ -48,7 +52,19 @@ const NewAd = () => {
       // const result = await axios.post("http://localhost:4000/ad", data);
       console.log("data form", data);
 
-      const result = await createNewAd({ variables: { adData: data } });
+      const result = await createNewAd({
+        variables: {
+          adData: {
+            title: data.title,
+            description: data.description,
+            imageUrl: imageURL,
+            location: data.location,
+            price: data.price,
+            owner: data.owner,
+            category: data.category,
+          },
+        },
+      });
       console.log("result", result);
       reset();
       router.push("/");
@@ -98,88 +114,129 @@ const NewAd = () => {
   // }, []);
 
   return (
-    <form
-      // onSubmit={(e) => {
-      //   // prevent browser from reloading the page;
-      //   e.preventDefault();
+    <>
+      <input
+        type="file"
+        onChange={(e) => {
+          if (e.target.files) {
+            setFile(e.target.files[0]);
+          }
+        }}
+      />
+      <button
+        onClick={async (event) => {
+          event.preventDefault();
+          if (file) {
+            const url = "http://localhost:8000/upload";
+            const formData = new FormData();
+            formData.append("file", file, file.name);
+            try {
+              const response = await axios.post(url, formData);
+              setImageURL(response.data.filename);
+            } catch (err) {
+              console.log("error", err);
+            }
+          } else {
+            alert("select a file to upload");
+          }
+        }}
+      >
+        Upload Image
+      </button>
+      {imageURL ? (
+        <>
+          <br />
+          <img
+            width={"500"}
+            alt="uploadedImg"
+            src={"http://localhost:8000" + imageURL}
+          />
+          <br />
+        </>
+      ) : null}
+      <form
+        // onSubmit={(e) => {
+        //   // prevent browser from reloading the page;
+        //   e.preventDefault();
 
-      //   //Read the form data
-      //   const form = e.target;
-      //   const formData = new FormData(form as HTMLFormElement);
+        //   //Read the form data
+        //   const form = e.target;
+        //   const formData = new FormData(form as HTMLFormElement);
 
-      //   const formJson = Object.fromEntries(formData.entries());
-      //   console.log(formJson);
-      //   axios.post("http://localhost:4000/ad", formJson)
-      // }}
-      onSubmit={handleSubmit(onSubmit)}
-    >
-      <div>
-        <label>
-          Titre de l&apos;annonce : <br />
-          <input
-            className="text-field"
-            {...register("title", { required: true })}
-          />
-          {errors.title && <span>This field is required</span>}
-        </label>
-      </div>
-      <div>
-        <label>
-          Prix : <br />
-          <input
-            className="text-field"
-            {...register("price", { required: true })}
-          />
-          {errors.price && <span>This field is required</span>}
-        </label>
-      </div>
-      <div>
-        <label>
-          Description : <br />
-          <input
-            className="text-field"
-            {...register("description", { required: true, maxLength: 200 })}
-          />
-          {errors.description && <span>This field is required</span>}
-        </label>
-      </div>
-      <div>
-        <label>
-          Ville : <br />
-          <input
-            className="text-field"
-            {...register("location", { required: true })}
-          />
-          {errors.location && <span>This field is required</span>}
-        </label>
-      </div>
-      <div>
-        <label>
-          Author : <br />
-          <input
-            className="text-field"
-            {...register("owner", { required: true })}
-          />
-          {errors.owner && <span>This field is required</span>}
-        </label>
-      </div>
-      <div>
-        <label>
-          Image : <br />
-          <input className="text-field" {...register("imageUrl")} />
-        </label>
-      </div>
-      <select {...register("category", { required: true })}>
-        {categories.map((el) => (
-          <option value={el.id} key={el.id}>
-            {el.name}
-          </option>
-        ))}
-      </select>
-      {errors.category && <span>This field is required</span>}
+        //   const formJson = Object.fromEntries(formData.entries());
+        //   console.log(formJson);
+        //   axios.post("http://localhost:4000/ad", formJson)
+        // }}
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <div>
+          <label>
+            Titre de l&apos;annonce : <br />
+            <input
+              className="text-field"
+              {...register("title", { required: true })}
+            />
+            {errors.title && <span>This field is required</span>}
+          </label>
+        </div>
+        <div>
+          <label>
+            Prix : <br />
+            <input
+              className="text-field"
+              {...register("price", { required: true })}
+            />
+            {errors.price && <span>This field is required</span>}
+          </label>
+        </div>
+        <div>
+          <label>
+            Description : <br />
+            <input
+              className="text-field"
+              {...register("description", { required: true, maxLength: 200 })}
+            />
+            {errors.description && <span>This field is required</span>}
+          </label>
+        </div>
+        <div>
+          <label>
+            Ville : <br />
+            <input
+              className="text-field"
+              {...register("location", { required: true })}
+            />
+            {errors.location && <span>This field is required</span>}
+          </label>
+        </div>
+        <div>
+          <label>
+            Author : <br />
+            <input
+              className="text-field"
+              {...register("owner", { required: true })}
+            />
+            {errors.owner && <span>This field is required</span>}
+          </label>
+        </div>
+        {/* <div>
+          <label>
+            Image : <br />
+            <input className="text-field" {...register("imageUrl")} />
+          </label>
+        </div> */}
+        <select {...register("category", { required: true })}>
+          {categories.map((el) => (
+            <option value={el.id} key={el.id}>
+              {el.name}
+            </option>
+          ))}
+        </select>
+        {errors.category && <span>This field is required</span>}
 
-      <button className="button">Submit</button>
-    </form>
+        <button className="button">Submit</button>
+      </form>
+    </>
   );
 };
 
